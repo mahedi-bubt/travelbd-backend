b4e1cd6795a8b94feeb75417052227efb1f43cc2
mahedi-bubt <mahedi.bubt.cse@gmail.com>
2021-11-02 01:31:19 +0600

    get all package and post data using ui done

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h1fbe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        /* console.log('connected to database'); */
+        const database = client.db('travelbd');
+        const packagesCollection = database.collection('packages');
+
+        //GET API
+        app.get('/packages', async (req, res) => {
+            const cursor = packagesCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        })
+
+        //POST API
+        app.post('/packages', async (req, res) => {
+            const package = req.body;
+            console.log('hit the post api', package);
+            const result = await packagesCollection.insertOne(package);
+            console.log(result);
+            res.json(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running my crud server');
+});
+
+app.listen(port, () => {
+    console.log('Running server port ', port);
+})
