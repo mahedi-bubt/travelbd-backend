20e810f83003d544a5b1023d01b1a4f25d4f9178
mahedi-bubt <mahedi.bubt.cse@gmail.com>
2021-11-03 17:21:29 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors')
 const app = express();
@@ -21,13 +22,31 @@ async function run() {
         /* console.log('connected to database'); */
         const database = client.db('travelbd');
         const packagesCollection = database.collection('packages');
+        const placeOrderCollection = database.collection('placeorder');
 
         //GET API
         app.get('/packages', async (req, res) => {
             const cursor = packagesCollection.find({});
             const packages = await cursor.toArray();
             res.send(packages);
-        })
+        });
+
+        //GET Single Services
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific id: ', id);
+            const query = { _id: ObjectId(id) }
+            const result = await packagesCollection.findOne(query);
+            res.json(result);
+
+        });
+
+        //GET PlaceOrder data
+        app.get('/placeorder', async (req, res) => {
+            const cursor = placeOrderCollection.find({});
+            const placeorders = await cursor.toArray();
+            res.send(placeorders);
+        });
 
         //POST API
         app.post('/packages', async (req, res) => {
@@ -35,6 +54,23 @@ async function run() {
             console.log('hit the post api', package);
             const result = await packagesCollection.insertOne(package);
             console.log(result);
+            res.json(result);
+        })
+
+        //PlaceOrder POST API
+        app.post('/placeorder', async (req, res) => {
+            const order = req.body;
+            console.log('hit the PlaceOrder post api', order);
+            const result = await placeOrderCollection.insertOne(order);
+            console.log(result);
+            res.json(result);
+        })
+
+        //DELETE API
+        app.delete('/placeorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await placeOrderCollection.deleteOne(query);
             res.json(result);
         })
 
